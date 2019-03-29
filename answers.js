d108365ac5daa49a5cbbfcc947ca51dcbb15f510
HeatherVicars <48963561+HeatherVicars@users.noreply.github.com>
2019-03-29 11:49:41 -0700

    Update answers.js

--- answers.js
+++ answers.js
@@ -6,11 +6,10 @@ var myName = 'Heather';
 
 //Print -52 to 1066. Print integers from -52 to 1066 using a FOR loop.
 
-for (var num = -52; num < 1067; num = num + 1);
+for (var num = -52; num < 1067; num = num + 1)
 {
    console.log(num);
 }
-console.log(num);
    
 //Don’t Worry, Be Happy. Create beCheerful(). Within it, console.log string "good morning!" Call it 98 times.
 
@@ -18,14 +17,14 @@ function beCheerful()
 {
    console.log("good morning!");
 }
-for (var num =1; num < 99; num = num +1)
+for (var num = 1; num < 99; num = num + 1)
 {
    beCheerful();
 }
 
 //Multiples of Three – but Not All. Using FOR, print multiples of 3 from -300 to 0. Skip -3 and -6.
-//for (var num = -300; num < 1; num = num + 3);
 
+for (var num = -300; num < 1; num = num + 3)
 {
    if (num == -6 || num == -3)
    {
@@ -36,11 +35,14 @@ for (var num =1; num < 99; num = num +1)
 
 //Printing Integers with While. Print integers from 2000 to 5280, using a WHILE.
 var num = 2000;
-while (num < 5281);
+
+while (num < 5281)
 {
-   console.log(num)
+   console.log(num);
+   
    num = num + 1;
 }
+console.log(num);
 
 //You Say It’s Your Birthday.  If 2 given numbers represent your birth month and day in either order, log 
 //"How did you know?", else log "Just another day..."
