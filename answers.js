2b3b09c16fb90f4670d0aed04b3f6c49329bc79c
HeatherVicars <48963561+HeatherVicars@users.noreply.github.com>
2019-03-29 07:51:23 -0700

    Update answers.js

--- answers.js
+++ answers.js
@@ -10,6 +10,7 @@ for (var num = -52; num < 1067; num = num + 1);
 {
    console.log(num);
 }
+console.log(num);
    
 //Don’t Worry, Be Happy. Create beCheerful(). Within it, console.log string "good morning!" Call it 98 times.
 
